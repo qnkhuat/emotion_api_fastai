ef74166b8d1daffcf72cdea0207bd539e4b5c89b
ngockq <qn.khuat@gmail.com>
2019-03-28 15:16:37 +0700

    add scripts to live emotion

---
+++ scripts/live_emotion.py
@@ -0,0 +1,25 @@
+import cv2
+import sys
+sys.path.insert(0,'../')
+
+from src.emotion.utils import load_model as load_emotion
+from src.detect_face.inference import load_detection_model as load_face
+from src.utils import *
+
+vid = cv2.VideoCapture(0)
+face_detector = load_face()
+emotion_detector = load_emotion()
+
+
+while True:
+    _,frame = vid.read()
+    image = bytes2array(request.data)
+    face,bbox = face_detector.find_biggest_face(image)
+    emotion = emotion_detector.predict_array(face)['emotion']
+    draw_bbox(image,bbox)
+    cv2.imshow(emotion,face)
+    if 0x00==orq('q') & cv2.waitkey(1):
+        break
+
+
+
